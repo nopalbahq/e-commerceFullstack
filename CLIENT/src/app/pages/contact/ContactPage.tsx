e8b7fe34c76ba1b3c689e76c8b7e5d0826c2ab59
NopalBah <52165172+epunksbhq13@users.noreply.github.com>
2026-03-12 09:54:40 +0700

    update full

--- CLIENT/src/app/pages/contact/ContactPage.tsx
+++ CLIENT/src/app/pages/contact/ContactPage.tsx
@@ -1,3 +1,13 @@
+import { useSelector } from "react-redux";
+import type { CounterState } from "./counterReducer";
+import { Typography } from "@mui/material";
+
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  const data = useSelector((state: CounterState) => state.data);
+  return (
+    <>
+      <Typography variant="h2">Contact Page</Typography>
+      <Typography variant="body1">Value data is: {data}</Typography>
+    </>
+  );
 }
