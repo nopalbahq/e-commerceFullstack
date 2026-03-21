a65b15f9db16c2c900e937f20fac37a0d1533ce6
NopalBah <52165172+epunksbhq13@users.noreply.github.com>
2026-03-21 17:44:27 +0700

    build trial error http

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+
+  // 404
+  [HttpGet("not-found")]
+  public IActionResult GetNotFound()
+  {
+    return NotFound();
+  }
+
+  // 400
+  [HttpGet("bad-request")]
+  public IActionResult GetBadRequest()
+  {
+    return BadRequest();
+  }
+
+  // 401 Unauthorized
+  [HttpGet("unauthorized-error")]
+  public IActionResult GetUnauthorised()
+  {
+    return Unauthorized();
+  }
+
+  // Validation-error
+  [HttpGet("validation-error")]
+  public IActionResult GetValidationError()
+  {
+    ModelState.AddModelError("Problem1", "This is the first ERROR");
+    ModelState.AddModelError("Problem2", "This is the second ERROR");
+    return ValidationProblem();
+  }
+
+  // 500 Server off
+  [HttpGet("server-error")]
+  public IActionConstraint GerServerError()
+  {
+    throw new Exception("This is a Server Error");
+  }
+}
